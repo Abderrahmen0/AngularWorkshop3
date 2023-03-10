c86ed1020f7f8f8156d820a92eb6fefb5c1dcddb
unknown <ghodhbaniabderrahmen@gmail.com>
2023-03-10 14:12:20 +0100

    first commit

---
+++ src/app/form-product/form-product.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../model/product';
+
+@Component({
+  selector: 'app-form-product',
+  templateUrl: './form-product.component.html',
+  styleUrls: ['./form-product.component.css']
+})
+export class FormProductComponent implements OnInit{
+  product!: Product;
+  list!:Product[];
+
+  ngOnInit(): void {
+  this.product=new Product();
+  this.list=[];
+  }
+  save(){
+    this.product.like=0;
+    this.list.push(this.product);
+    console.log('after the click');
+    console.log(this.list);
+  }
+}
